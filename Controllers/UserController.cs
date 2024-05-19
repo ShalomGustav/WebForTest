45dc626cc60fd8f8b3c03609a4716fcdda4cfba0
Максим Сергеевич Фатихов <maximvaryagov@gmail.com>
2024-05-19 21:38:31 +0300

    Дописать логику  UserByLoginController

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,108 +1,107 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using WebForTest.Models;
 using WebForTest.Services;
 
-namespace WebForTest.Controllers
+namespace WebForTest.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class UserController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    
-    public class UserController : ControllerBase
+    private readonly UserService _userService;
+
+    public UserController(UserService userService)
     {
-        private readonly UserService _userService;
-
-        public UserController(UserService userService)
-        {
-            _userService = userService;
-        }
-
-        //[HttpGet("user-active")]
-        //[ProducesResponseType(typeof(User),200)]
-        //public IActionResult GetActive()
-        //{
-        //    var users = _userService.GetActiveUsers();
-        //    return Ok(users);
-        //}
-        #region HttpGet
-        [HttpGet("user-active")]
-        public List<User> GetActive()
-        {
-            var users = _userService.GetActiveUsers();
-            return users;
-        }
-
-        [HttpGet("user-by-login")]
-        public User GetByLogin([FromQuery] string login)
-        {
-            var userLogin = _userService.GetUserByLogin(login);
-            return userLogin;
-        }
-
-        [HttpGet("user-by-login-pass")]
-        public User GetByLoginAndPass([FromQuery]string login,string pass)
-        {
-            var loginAndPass = _userService.GetUserByLoginAndPass(login, pass);
-            return loginAndPass;
-        }
-
-        [HttpGet("user-by-birthday")]
-        public List<User> GetByBirthDay([FromQuery]int age)
-        {
-            var birthDay = _userService.GetUserByBirthDay(age);
-            return birthDay;
-        }
-        #endregion
-
-        #region HttpPost
-
-        [HttpPost("create-user-by-name-login-pass")]
-        public void Create([FromQuery] User user)
-        {
-            _userService.CreateUser(user);
-        }
-
-        #endregion
-
-        #region HttpPut
-        [HttpPut("update")]
-        public User Update([FromQuery] User user)
-        {
-            return _userService.UpdateUser(user);
-        }
-
-        [HttpPut("change-name-gen-birthday")]
-        public void СhangeNameGenBirthDay([FromQuery] string login, User user)
-        {
-             _userService.СhangeUserNameGenBirthDay(login,user);
-        }
-
-        [HttpPut("change-password")]
-        public void СhangePassword([FromQuery] string login, string password)
-        {
-            _userService.СhangePasswordUser(login,password);
-        }
-
-        [HttpPut("change-login")]
-        public void СhangeUser([FromQuery] string login)
-        {
-            _userService.СhangeLoginUser(login);
-        }
-
-        [HttpPut("recovery-user")]
-        public void Recovery([FromQuery] string login)
-        {
-            _userService.RecoveryUser(login);
-        }
-
-        #endregion
-
-        #region HttpDelete
-        [HttpDelete("delete-by-login")]
-        public void DeleteByLogin([FromQuery] string login,bool softRemove)
-        {
-            _userService.DeleteUserByLogin(login,softRemove);
-        }
-        #endregion
+        _userService = userService;
     }
+
+    #region HttpGet
+    [HttpGet("user-active")]
+    public List<UserEntity> GetActive()
+    {
+        var users = _userService.GetActiveUsers();
+        return users;
+    }
+
+    [HttpGet("user-current")]
+    public UserEntity GetCurrentUser()
+    {
+        var result = _userService.GetCurrentUser();
+        return result;
+    }
+
+    [HttpGet("logout")]
+    public ActionResult Logout()
+    {
+        _userService.Logout();
+        return Ok();
+    }
+
+    [HttpGet("user-by-login")]
+    public UserEntity GetByLogin([FromQuery] string login)
+    {
+        var userLogin = _userService.GetUserByLogin(login);
+        return userLogin;
+    }
+
+    [HttpGet("login")]
+    public ActionResult Login([FromQuery]string login,string pass)
+    {
+        _userService.Login(login, pass);
+        return Ok();
+    }
+
+    [HttpGet("user-by-birthday")]
+    public List<UserEntity> GetByBirthday([FromQuery]int age)
+    {
+        var birthDay = _userService.GetUserByBirthday(age);
+        return birthDay;
+    }
+    #endregion
+
+    #region HttpPost
+
+    [HttpPost("create-user")]
+    public void CreateUser([FromBody] UserDetails userDetails)
+    {
+        _userService.CreateUser(userDetails);
+    }
+
+    #endregion
+
+    #region HttpPut
+    [HttpPut("change-user")]
+    public void СhangeUser([FromBody] UserEntity user)
+    {
+         _userService.СhangeUser(user);
+    }
+
+    [HttpPut("change-password")]
+    public void СhangePassword([FromQuery] string login, string password)
+    {
+        _userService.СhangePasswordUser(login,password);
+    }
+
+    [HttpPut("change-login")]
+    public void СhangeLogin([FromQuery] string oldLogin, string newLogin)
+    {
+        _userService.СhangeLoginUser(oldLogin, newLogin);
+    }
+
+    [HttpPut("recovery-user")]
+    public void RecoveryUser([FromQuery] string login)
+    {
+        _userService.RecoveryUser(login);
+    }
+
+    #endregion
+
+    #region HttpDelete
+    [HttpDelete("delete-by-login")]
+    public void DeleteByLogin([FromQuery] string login,bool softRemove)
+    {
+        _userService.DeleteUserByLogin(login,softRemove);
+    }
+    #endregion
 }
--- Program.cs
+++ Program.cs
@@ -9,8 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<UserService>(); // –егистраци€ сервиса UserService в контейнере зависимостей
-
+builder.Services.AddSingleton<UserService>(); 
 
 var app = builder.Build();
 
